b95d3a231536f2f6bdf8db38c9ff34cf21e6f569
ArgentZD <80226017+ArgentZD@users.noreply.github.com>
2022-12-02 22:47:02 -0600

    Update Sergio_Morales-hardening.sh

--- Sergio_Morales-hardening.sh
+++ Sergio_Morales-hardening.sh
@@ -15,17 +15,19 @@ then
                 echo "INSTALLING CLAMAV"
                 sudo yum install clamav -y
         else
-            	  echo "REINSTALLING CLAMAV"
+            	echo "REINSTALLING CLAMAV"
                 sudo yum remove clamav -y
                 sudo yum install clamav -y
         fi
-        #Install EPEL repository
-	      echo "REPEL INSTALLATION"
+        
+	#Install EPEL repository
+	echo "REPEL INSTALLATION"
         sudo yum -y install epel-release
         yum repo list
         sudo yum --disablerepo="*" --enablerepo="epel" list available
-        #Update repos
-	      echo "UPDATE"	
+        
+	#Update repos
+	echo "UPDATE"	
         sudo yum update -y
 
 elif echo "$OS" | grep "ubuntu";
@@ -34,15 +36,16 @@ then
        #Clamav
        if echo  "$detect2" | grep -m 1 "no such file or directory";
         then
-               echo "installing clamav"
+               	echo "installing clamav"
                 sudo apt-get install clamav -y
         else
                 echo "REMOVING CLAMAV"
                 sudo apt-get remove clamav clamav-daemon -y
-		            sudo apt-get autoremove clamav clamav-daemon -y
-		            echo "INSTALLING CLAMAV"
+		sudo apt-get autoremove clamav clamav-daemon -y
+		
+		echo "INSTALLING CLAMAV"
                 sudo apt-get install clamav clamav-daemon -y
         fi
-			  echo "SYSTEM UPDATE"
-			  sudo apt-get update -y
+		echo "SYSTEM UPDATE"
+		sudo apt-get update -y
 fi
